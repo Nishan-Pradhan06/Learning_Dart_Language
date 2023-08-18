5bb634499dcc9ba813bdd1d8de97a90875701b86
nishan <nishanpradhan500@gmail.com>
2023-08-18 18:15:11 +0545

    functions

--- day_7/17function_NR_NP.dart
+++ day_7/17function_NR_NP.dart
@@ -1,8 +1,36 @@
 // no return type and no parameter fuction
 void main(List<String> args) {
   defaultFUnction(); //function call
+  int sum = add(1, 8);
+
+  int sub = diff(2, 5);
+  print(sum);
+  print(sub);
+
+  newAdd(33, 2);
 }
+
 // function
 void defaultFUnction() {
-  print("No return type and no parameter fuction");
+  print("return type and with parameter fuction");
 }
+
+int add(a, b) {
+  return (a + b);
+}
+
+int diff(a, b) {
+  return (a - b);
+}
+
+void newAdd(a, b, [c]) { // in dart we can add another varibale as optional in functions using [c]
+  int sum = a + b;
+  print("sum is "+sum.toString());
+  print(a);
+  print(b);
+  print(c);
+}
+//null mean blank purai khaliii
+//empty "" nothing inside the quote
+//zero mean 
+// all three are different
